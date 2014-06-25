e4476ea905a091498b8c8556469dddee37f37570
sourcebits-rbiggs <robert.biggs@sourcebits.com>
2014-06-25 09:48:31 -0700

    Adjust for ChocolateChip-UI custom events.
    Needed to adjust the test for presence of ChocolateChip-UI to avoid
    throwing errors.

--- src/2.settings.js
+++ src/2.settings.js
@@ -62,8 +62,8 @@
 	settings.eventsPrefix = 'data-';
 	var eventsString = 'click dblclick mousedown mouseup mouseover mouseout mousemove mouseenter mouseleave keydown keyup focus blur change select selectstart scroll copy cut paste mousewheel keypress error contextmenu input textinput drag dragenter dragleave dragover dragend dragstart dragover drop load submit reset search resize beforepaste beforecut beforecopy';
 	eventsString += ' touchstart touchend touchmove touchenter touchleave touchcancel gesturestart gesturechange gestureend';
-	if (window && window.$ && window.$.eventStart) {
-		eventString += ' tap singletap doubletap longtap swipe swiperight swipeleft swipeup swipedown';
+	if ($ && $.eventStart !== undefined) {
+		eventsString += ' tap singletap doubletap longtap swipe swipeleft swiperight swipeup swipedown';
 	}
 	var eventsArray = eventsString.split(' ');
 	var i = -1, l = eventsArray.length;
